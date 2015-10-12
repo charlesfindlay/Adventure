552a8512e26a83c913e96ae3600f90c79f8305f5
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-10-12 15:11:28 -0400

    Models created for adventure, storynodes, and connections

---
+++ Adventure/Connection.swift
@@ -0,0 +1,15 @@
+//
+//  Connection.swift
+//  Adventure
+//
+//  Created by Student on 10/12/15.
+//  Copyright © 2015 Charles Findlay. All rights reserved.
+//
+
+import Foundation
+
+class Connection {
+    var prompt: String!
+    var connectedToNode: String!
+    
+}
---
+++ Adventure/StoryNode.swift
@@ -0,0 +1,15 @@
+//
+//  StoryNode.swift
+//  Adventure
+//
+//  Created by Student on 10/12/15.
+//  Copyright © 2015 Charles Findlay. All rights reserved.
+//
+
+import Foundation
+
+class StoryNode {
+    var storyText: String!
+    var connections: [Connection]
+    
+}
